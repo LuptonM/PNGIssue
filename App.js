b8c7fdc50e9c9b4badc1d68f17e6783735bd1ecc
Marc Lupton <marc.lupton100@gmail.com>
2021-04-07 14:52:32 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+
+const beer =require("./Graphics/beer.png")
+const stars =require("./Graphics/star.png")
 
 export default function App() {
   return (
     <View style={styles.container}>
+	
+	<View style={{flex:1, width:"100%", height:"100%", alignContent:"center", justifyContent:"center",alignItems: 'center'}}>
       <Text>Open up App.js to start working on your app!</Text>
+	  </View>
       <StatusBar style="auto" />
+	  
+	  <View style={{flex:1, width:"100%", height:"100%", alignContent:"center", justifyContent:"center",}}>
+	  
+	  <View style={{backgroundColor:"grey"}}>
+	   <Image src={beer} style={{height:100, width:60}}/>
+	   </View>
+		 <View style={{backgroundColor:"yellow"}}>
+		<Image src={stars} style={{height:100, width:200}}/>
+		</View>
+		</View>
     </View>
   );
 }
@@ -18,4 +36,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+ 
 });
